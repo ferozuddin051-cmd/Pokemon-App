710b31d13f300d32d72ba88c457f52ff55ad02ba
ferozuddin051-cmd <ferozuddin051@gmail.com>
2025-12-27 21:43:23 +0500

    Commit with fetched API

---
+++ src/Pokemon.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react"
+import "./index.css"
+
+export const Pokemon = () => {
+    const API = "https://pokeapi.co/api/v2/pokemon?limit=24";
+    
+    const fetchPokemon = async () => {
+        try {
+            const res = await fetch(API);
+            const data = await res.json();
+            console.log(data);
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchPokemon();
+    }, [])
+
+    return(
+        <>
+        <h1>Hello Pokemon Once Again</h1>
+        </>
+    )
+}
